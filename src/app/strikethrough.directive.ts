8c0ac0effe5959ebbc4df283e9c900f2d891daa0
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-01 22:48:10 +0300

    added strikethrough on single click and none strikethrough on double click

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
 
-  constructor() { }
+  constructor(private Elem:ElementRef) {}
+
+  @HostListener('click') onClicks(){
+    this.textDeco('line-through')
+  }
+  @HostListener('dblclick') onDoubleClicks(){
+    this.textDeco('None')
+  }
+  
+  private textDeco(Action:string){
+    this.Elem.nativeElement.style.textDecoration=Action;
+  }
 
 }
