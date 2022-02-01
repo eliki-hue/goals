4a4d6617ababc022477ac1f1c461912dc4142aa1
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-01 11:31:30 +0300

    imported goorted Goal class in goals.component.ts

--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Goal} from '../goal'
 
 @Component({
   selector: 'app-goals',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goals.component.css']
 })
 export class GoalsComponent implements OnInit {
+  goals: Goal[] =[
+    {id:1, name:'watch finding Nemo '},
+    {id:2, name: 'Buy cookies'},
+    {id:3, name:'get new Phone case'},
+    {id:4, name :'Get dogs food'},
+    {id:5, name: 'Solve math problem'},
+    {id:6, name :'Plot my world domination plan'}
+  ];
 
   constructor() { }
 
