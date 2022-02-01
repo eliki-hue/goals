03a363c5785cd283fcb65c481936c54e8d5dbbb2
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-01 12:16:23 +0300

    generated goal-detail component add imported class Goal from goals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalsComponent } from './goals/goals.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalsComponent
+    GoalsComponent,
+    GoalDetailComponent
   ],
   imports: [
     BrowserModule,
