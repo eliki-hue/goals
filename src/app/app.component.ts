3a6963e966cfe71c22769f2da12d020a55f54b23
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-01 12:04:51 +0300

    added description property to Goal class and added description to each goal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Goals';
   goals: Goal[] =[
-    {id:1, name:'watch finding Nemo '},
-    {id:2, name: 'Buy cookies'},
-    {id:3, name:'get new Phone case'},
-    {id:4, name :'Get dogs food'},
-    {id:5, name: 'Solve math problem'},
-    {id:6, name :'Plot my world domination plan'}
+    {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
+    {id:2,name:'Buy Cookies',description:'I have to buy cookies for the parrot'},
+    {id:3,name:'Get new Phone Case',description:'Diana has her birthday coming up soon'},
+    {id:4,name:'Get Dog Food',description:'Pupper likes expensive sancks'},
+    {id:5,name:'Solve math homework',description:'Damn Math'},
+    {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
   ];
 }
 
